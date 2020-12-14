e02b416951d8ac632cd7e80f61563c1b8b18e152
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-12-14 14:23:35 -0500

    Add sidebar image and background

--- src/components/Sidebar/Sidebar.styles.js
+++ src/components/Sidebar/Sidebar.styles.js
@@ -2,5 +2,17 @@ import styled from "@emotion/styled";
 
 export const SidebarContainer = styled.div`
   width: 20%;
-  background: yellow;
+  max-width: 280px;
+  min-width: 80px;
+  background-image: linear-gradient(
+      315deg,
+      rgb(189, 216, 254, 0.8) 0%,
+      rgb(225, 134, 180, 0.8) 74%
+    ),
+    url("images/company.jpeg");
+  background-size: cover;
+  background-repeat:no-repeat;
+  background-position: center center;
+  
+
 `;
